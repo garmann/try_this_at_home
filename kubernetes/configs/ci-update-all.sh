0bec72027637e5a94d1bd762066ade34c98d9fbe
Gregor Armann <gregor.armann@googlemail.com>
2017-10-10 17:58:47 +0200

    changed kubectl cmd from replace to apply

--- kubernetes/configs/ci-update-all.sh
+++ kubernetes/configs/ci-update-all.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-kubectl replace -f ./http-status-code/deployment.yaml
-kubectl replace -f ./http-status-code/service.yaml
+kubectl apply -f ./http-status-code/deployment.yaml
+kubectl apply -f ./http-status-code/service.yaml
 
-kubectl replace -f ./myip/deployment.yaml
-kubectl replace -f ./myip/service.yaml
+kubectl apply -f ./myip/deployment.yaml
+kubectl apply -f ./myip/service.yaml
 
-kubectl replace -f ./useragent/deployment.yaml
-kubectl replace -f ./useragent/service.yaml
+kubectl apply -f ./useragent/deployment.yaml
+kubectl apply -f ./useragent/service.yaml
 
-kubectl replace -f ingress.yaml
+kubectl apply -f ingress.yaml
